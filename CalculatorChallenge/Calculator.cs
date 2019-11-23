2a3515f83a1d1c27d452d6b1a349228f53584541
Faraz K <faraz_khalid21@hotmail.com>
2019-11-23 15:09:21 -0800

    Task #1 Completed
    Support a maximum of 2 numbers using a comma delimiter. Throw an exception when more than 2 numbers are provided
    examples: 20 will return 20; 1,5000 will return 5001; 4,-3 will return 1
    empty input or missing numbers should be converted to 0
    invalid numbers should be converted to 0 e.g. 5,tytyt will return 5

---
+++ CalculatorChallenge/Calculator.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorChallenge
+{
+    public class Calculator
+    {
+        private string _numberList;
+        private int[] _numbers;
+
+        public Calculator(string values)
+        {
+            _numberList = values;
+        }
+
+        public string validateNumber()
+        {
+            string isValid = "valid";
+
+            int tempValue = 0;
+
+
+            if (_numberList.Length > 0)
+            {
+                if (_numberList.IndexOf(",") >= 0)
+                {
+                    string[] strNumbers = _numberList.Split(",".ToCharArray());
+
+                    if (strNumbers.Length <= 2)
+                    {
+                        _numbers = new int[strNumbers.Length];
+
+                        int counter = 0;
+
+                        foreach (var valu in strNumbers)
+                        {
+                            int.TryParse(valu, out tempValue);
+
+                            _numbers[counter] = tempValue;
+
+                            counter++;
+                        }
+                    }
+                    else
+                    {
+                        isValid = "You entered more than 2 numbers. Please enter only two numbers (,) seprated.";
+                    }
+                }
+                else if (_numberList.Length > 0)
+                {
+                    _numbers = new int[1];
+
+                    int.TryParse(_numberList, out tempValue);
+
+                    _numbers[0] = tempValue;
+                }
+                else
+                {
+                    isValid = "Number list is empty";
+                }
+
+            }
+
+
+            return isValid;
+        }
+        public int addNumber()
+        {
+            var result = 0;
+            if (_numbers.Length > 0)
+            {
+                for (int i = 0; i < _numbers.Length; i++)
+                {
+                    result += _numbers[i];
+                }
+            }
+
+            return result;
+        }
+
+        public double subtractNumber(int[] numbers)
+        {
+            return 0.0;
+        }
+
+        public double multiplyNumber(int[] numbers)
+        {
+            return 0.0;
+        }
+
+        public double divideNumber(int[] numbers)
+        {
+            return 0.0;
+        }
+    }
+}
---
+++ CalculatorChallenge/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CalculatorChallenge
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            ConsoleKeyInfo keyInfo;
+            // Establish an event handler to process key press events.
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(exitHandler);
+
+            Console.WriteLine(@"CTRL+C to Cancel");
+
+            Console.WriteLine("########################################################");
+            Console.WriteLine("");
+
+            while (true)
+            {
+                Console.WriteLine("Enter your numbers array seprated by (,) for Arithmatic operation:");
+                var array = Console.ReadLine();
+
+                Calculator calc = new Calculator(array);
+
+                var result = calc.validateNumber();
+
+                if (result == "valid")
+                {
+                    var sum = calc.addNumber();
+
+                    Console.WriteLine(sum);
+                }
+                else
+                {
+                    Console.WriteLine(result);
+                }
+
+                keyInfo = Console.ReadKey(true);
+            }
+        }
+
+        protected static void exitHandler(object sender, ConsoleCancelEventArgs args)
+        {
+            if (args.SpecialKey.ToString().ToLower() == "controlc")
+            {
+                Console.WriteLine("\nThe arthimatic operation has been interrupted.");
+                args.Cancel = true;
+                Environment.Exit(0);
+            }
+
+        }
+    }
+}
